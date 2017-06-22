a7b2b473e7896dd5b46bcdf4983385e6ceb56adc
Andreas Schönefeldt <schoenefedt.andreas@gmail.com>
2017-06-22 08:56:14 +0200

    Fixed tests

--- test/test.js
+++ test/test.js
@@ -2,14 +2,17 @@
 
 const expect = require('chai').expect
 const sheetTranslationSync = require('../index')
+const connector = require('../src/connector')
 const accessData = require('./data/google-test-access.json')
 
 const testSheetId = "1ZJK1G_3wrEo9lnu1FenjOzSy3uoAi-RLWbph1cI6DWI"
 
 describe('#translationSync', function () {
   it('should connect to the test google doc', function () {
-    let result = sheetTranslationSync(testSheetId, accessData)
-    expect(result).to.be.true
+    connector(testSheetId, accessData, function (sheet) {
+      expect(sheet).to.be.an('object')
+    })
+
   })
 
 
